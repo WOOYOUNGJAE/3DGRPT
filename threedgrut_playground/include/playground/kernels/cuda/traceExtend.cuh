1db010b2ca87069703873be4e30d68e8aeb01adb
WOO YOUNGJAE <azn2ww@naver.com>
2025-05-16 19:22:11 +0900

    [youngjae] shadow on Gaussian

--- threedgrut_playground/include/playground/kernels/cuda/traceExtend.cuh
+++ threedgrut_playground/include/playground/kernels/cuda/traceExtend.cuh
@@ -12,4 +12,42 @@ static __device__ __forceinline__ void packPointer(void* ptr, unsigned int& i0)
     i0 = uptr >> 32;
 }
 
+static __device__ __forceinline__ unsigned int traceOcclusion(const float3 rayOri, const float3 rayDir)
+{
+    unsigned int is_occluded;
+    HybridRayPayload occlusionPayload;
+    packPointer(&occlusionPayload, is_occluded);
+    is_occluded = 0u;
+
+    // check occluded by mesh
+    optixTrace(
+        params.triHandle,
+        rayOri,
+        rayDir,
+        TRACE_MESH_TMIN,
+        TRACE_MESH_TMAX, 0.0f,                // rayTime
+        OptixVisibilityMask( 255 ),
+        OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT | OPTIX_RAY_FLAG_DISABLE_ANYHIT,
+        1,                         // SBT offset
+        1,                          // SBT stride
+        0,                          // missSBTIndex,
+        is_occluded
+        );
+    // check occluded by Gaussians
+    optixTrace(
+        params.handle,
+        rayOri,
+        rayDir,
+        TRACE_MESH_TMIN,
+        TRACE_MESH_TMAX, 0.0f,                // rayTime
+        OptixVisibilityMask( 255 ),
+        OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT | OPTIX_RAY_FLAG_DISABLE_ANYHIT,
+        1,                         // SBT offset
+        1,                          // SBT stride
+        0,                          // missSBTIndex,
+        is_occluded
+        );
+    return is_occluded;
+}
+
 #endif
